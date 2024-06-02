3d560293feee7b7f12876b33f3264c6752c3ff5e
styanr <chaus.oleh@gmail.com>
2024-06-02 19:56:20 +0300

    feat: add ability to add users to group chat

--- chat-client/src/components/ViewGroupChatModal.tsx
+++ chat-client/src/components/ViewGroupChatModal.tsx
@@ -9,6 +9,8 @@ import {
 
 import { useGetUserByIdQuery } from "../features/users/usersApiSlice"
 
+import { useGetContactsQuery } from "../features/contacts/contactsApiSlice"
+
 import ProfileImage from "../components/ProfileImage"
 
 import { ChatRoomDetails } from "../features/chatrooms/chatRoomApiSlice"
@@ -26,6 +28,8 @@ const ViewGroupChatModal: FC<ViewGroupChatModalProps> = ({
   onClose,
 }) => {
   const [isEditing, setIsEditing] = useState(false)
+  const [isAddingUsers, setIsAddingUsers] = useState(false)
+
   return (
     <div className="fixed inset-0 bg-slate-800 shadow-lg p-6 w-full z-20 flex items-center justify-center">
       <div className="max-w-md w-full h-full">
@@ -36,6 +40,12 @@ const ViewGroupChatModal: FC<ViewGroupChatModalProps> = ({
           />
         ) : (
           <>
+            {isAddingUsers && (
+              <AddUsersToGroupChatModal
+                chatRoom={chatRoom}
+                onClose={() => setIsAddingUsers(false)}
+              />
+            )}
             <div className="flex justify-between items-center">
               <div className="flex items-center gap-4">
                 <ProfileImage src={chatRoom.pictureUrl} size={20} />
@@ -60,7 +70,15 @@ const ViewGroupChatModal: FC<ViewGroupChatModalProps> = ({
               {chatRoom.description || "No description"}
             </p>
             <div>
-              <h3 className="text-lg font-semibold mt-4">Members</h3>
+              <div className="flex justify-between items-center mt-4">
+                <h3 className="text-lg font-semibold">Members</h3>
+                <button
+                  onClick={() => setIsAddingUsers(true)}
+                  className="text-white hover:bg-slate-700 rounded-lg transition-colors duration-300 p-3"
+                >
+                  <RiPencilFill className="w-5 h-5" />
+                </button>
+              </div>
               <div className="flex flex-col mt-2 gap-2">
                 {chatRoom.userIds.map(userId => (
                   <UserListItem key={userId} userId={userId} />
@@ -154,6 +172,98 @@ const EditGroupChatModal: FC<ViewGroupChatModalProps> = ({
         >
           {isLoading ? "Saving..." : "Save"}
         </button>
+      </div>
+    </div>
+  )
+}
+
+interface ViewGroupChatModalProps {
+  chatRoom: ChatRoomDetails
+  onClose: () => void
+}
+
+const AddUsersToGroupChatModal: FC<ViewGroupChatModalProps> = ({
+  chatRoom,
+  onClose,
+}) => {
+  const [addUsersToGroupChatRoom, { isLoading }] =
+    useAddUsersToGroupChatRoomMutation()
+  const [userIds, setUserIds] = useState<string[]>([])
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+  const {
+    data: contacts,
+    isLoading: isContactsLoading,
+    error: contactsError,
+  } = useGetContactsQuery()
+
+  const onSave = async () => {
+    try {
+      await addUsersToGroupChatRoom({ id: chatRoom.id, userIds }).unwrap()
+      onClose()
+    } catch (error) {
+      setErrorMessage((error as any).message)
+    }
+  }
+
+  if (isContactsLoading || !contacts) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 z-30">
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <div className="bg-slate-800 p-6 rounded-lg">
+          <h2 className="text-white text-lg font-semibold">Add Users</h2>
+
+          <div className="flex flex-col gap-2 mt-4">
+            {contacts.users.map(contact => (
+              <label
+                key={contact.id}
+                className={`flex items-center justify-between gap-2 py-3 px-5 ${userIds.includes(contact.id) ? "bg-slate-700" : ""} rounded-lg transition-colors duration-300 min-w-80`}
+              >
+                <div className="flex items-center gap-2">
+                  <ProfileImage src={contact.profilePictureUrl} size={10} />
+                  <p className="ml-2">{contact.displayName}</p>
+                </div>
+
+                <input
+                  type="checkbox"
+                  checked={userIds.includes(contact.id)}
+                  onChange={e => {
+                    if (e.target.checked) {
+                      setUserIds([...userIds, contact.id])
+                    } else {
+                      setUserIds(userIds.filter(id => id !== contact.id))
+                    }
+                  }}
+                />
+              </label>
+            ))}
+          </div>
+
+          {errorMessage && (
+            <p className="text-red-500 mt-2">
+              Failed to add users: {errorMessage}
+            </p>
+          )}
+
+          <div className="flex justify-end mt-4">
+            <button
+              onClick={onClose}
+              className="bg-red-500 text-white rounded-lg py-2 px-3 mt-4 flex justify-center items-center hover:bg-red-600 transition-colors duration-300"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={onSave}
+              disabled={isLoading}
+              className="bg-green-500 text-white rounded-lg py-2 px-3 mt-4 flex justify-center items-center hover:bg-green-600 transition-colors duration-300 ml-2"
+            >
+              {isLoading ? "Adding..." : "Add"}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
